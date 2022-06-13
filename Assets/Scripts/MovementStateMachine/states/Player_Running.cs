731eec3d96457c82b30f80e2e04af95d77779c33
Jonah Ryan <87236765+jonahrobot@users.noreply.github.com>
2022-06-13 15:06:04 -0500

    Mario moves faster down hill and slower up hill!
    Bug fixed with running down hill feeling more like falling down hill!

--- Assets/Scripts/MovementStateMachine/states/Player_Running.cs
+++ Assets/Scripts/MovementStateMachine/states/Player_Running.cs
@@ -13,6 +13,7 @@ public class Player_Running : Player_State
     private float CurrentSprintSpeed; // Running
     private float TurnSpeed = 0.1f; // Running
 
+
     public Player_Running(PlayerStateMachineCore core)
     {
         this.core = core;
@@ -32,42 +33,94 @@ public class Player_Running : Player_State
 
     public override void UpdateMethod()
     {
-            float TargetAngle = Mathf.Atan2(core.movementInput.x, core.movementInput.y) * Mathf.Rad2Deg + core.Camera.eulerAngles.y;
-            float CurrentAngle = Mathf.SmoothDampAngle(core.transform.eulerAngles.y, TargetAngle, ref turnSmoothVelocity, TurnSpeed);
+        float TargetAngle = Mathf.Atan2(core.movementInput.x, core.movementInput.y) * Mathf.Rad2Deg + core.Camera.eulerAngles.y;
+        float CurrentAngle = Mathf.SmoothDampAngle(core.transform.eulerAngles.y, TargetAngle, ref turnSmoothVelocity, TurnSpeed);
 
-            // Set Angles into range of 0-360
+        // Removed slow down thing
+        /*
+                    // Set Angles into range of 0-360
 
-            var TargetRefined = TargetAngle % 360;
-            if (TargetRefined < 0) { TargetRefined += 360; }
+                    var TargetRefined = TargetAngle % 360;
+                    if (TargetRefined < 0) { TargetRefined += 360; }
 
-            var CurrentRefined = CurrentAngle % 360;
-            if (CurrentRefined < 0) { CurrentRefined += 360; }
+                    var CurrentRefined = CurrentAngle % 360;
+                    if (CurrentRefined < 0) { CurrentRefined += 360; }
 
-            var DifferenceChange = 180;
-            if (CurrentSprintSpeed < MaxSprintSpeed * 0.75)
-            {
-                DifferenceChange = 20;
-            }
+                    var DifferenceChange = 180;
+                    if (CurrentSprintSpeed < MaxSprintSpeed * 0.75)
+                    {
+                        DifferenceChange = 20;
+                    }
 
-            // Slow Player on Quick Direction Switch
+                    // Slow Player on Quick Direction Switch
 
-            if (Mathf.Abs(TargetRefined - CurrentRefined) > DifferenceChange)
-            {
-            CurrentSprintSpeed = Mathf.Max(CurrentSprintSpeed * 0.5f, BaseSprintSpeed);
-            }
+                    if (Mathf.Abs(TargetRefined - CurrentRefined) > DifferenceChange)
+                    {
+                    CurrentSprintSpeed = Mathf.Max(CurrentSprintSpeed * 0.5f, BaseSprintSpeed);
+                    }
+        */
 
         core.transform.rotation = Quaternion.Euler(0f, CurrentAngle, 0f);
 
-            Vector3 Direction = Quaternion.Euler(0f, TargetAngle, 0f) * Vector3.forward;
+        Vector3 Direction = Quaternion.Euler(0f, TargetAngle, 0f) * Vector3.forward;
 
-            // Accelerate player
-            if (CurrentSprintSpeed < MaxSprintSpeed)
-            {
+        // Accelerate player
+        if (CurrentSprintSpeed < MaxSprintSpeed)
+        {
             CurrentSprintSpeed *= SprintAcceleration;
-            }
+        }
+        else
+        {
+            CurrentSprintSpeed /= SprintAcceleration;
+        }
+
+        Direction = SlopeFix(Direction, core.transform.position);
 
         core.Character.Move(Direction.normalized * CurrentSprintSpeed * Time.deltaTime);
     }
 
+    private Vector3 SlopeFix(Vector3 v, Vector3 pos)
+    {
+        var raycast = new Ray(pos, Vector3.down);
 
+        if (Physics.Raycast(raycast, out RaycastHit hitInfo, 2f))
+        {
+            // Get normal
+            var slopeRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal); // The direction needed for correction
+
+            var adjustVel = slopeRotation * v; // This rotates the players direction vector to the direction perpendicular to the hill
+
+            Debug.DrawLine(hitInfo.point + hitInfo.normal, hitInfo.point, Color.cyan);
+
+            if (adjustVel.y < -0.2f)
+            {
+                Debug.Log("Running Down Hill");
+
+                SprintAcceleration = 1.00125f * 2f;
+                MaxSprintSpeed = 28f;
+
+                return adjustVel;
+            }
+            else
+            {
+                if (adjustVel.y > 0.2f)
+                {
+                    Debug.Log("Running UP THE HILL");
+                    MaxSprintSpeed = 20f;
+
+                    return adjustVel;
+                }
+                else
+                {
+                    Debug.Log("At flat ground");
+
+                    SprintAcceleration = 1.00125f;
+                    MaxSprintSpeed = 25f;
+
+                }
+            }
+        }
+        return v;
+
+    }
 }
